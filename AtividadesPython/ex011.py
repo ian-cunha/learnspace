8ac51e47ca807b5086c397aaeeb2a11a220b321b
Ian <iancunha20@gmail.com>
2024-02-22 19:57:14 -0300

    update

---
+++ AtividadesPython/ex011.py
@@ -0,0 +1,5 @@
+# Desafio 011
+# Faça um programa que leia a largura e a altura de uma parede em metros,
+# calcule a sua área e a quantidade de tinta necessário para pintá-la,
+# sabendo que cada litro de tinta, pinta uma área de 2m½ (2 metros quadrados).
+
