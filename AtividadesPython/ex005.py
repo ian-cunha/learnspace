6695ae98d04aa076985af5c7b2055da9230dbf98
Ian <iancunha20@gmail.com>
2024-02-23 07:07:09 -0300

    update

--- AtividadesPython/ex005.py
+++ AtividadesPython/ex005.py
@@ -2,3 +2,7 @@
 # Faça um programa que leia um número inteiro e 
 # mostre na tela o seu sucessor e seu antecessor.
 
+numero = int(input('Digite um número: '))
+anterior = numero - 1
+proximo = numero + 1
+print('O número anterior é {}, e o próximo é {}.'.format(anterior, proximo))
--- AtividadesPython/ex006.py
+++ AtividadesPython/ex006.py
@@ -1,4 +1,12 @@
 # Desafio 006
 # Crie um algoritmo que leia um número e mostre o seu dobro,
 # triplo e raiz quadrada.
+import math
 
+n = int(input('Digite algum número: '))
+dobro = n + n
+triplo = n + n + n
+raiz = math.sqrt(n)
+print('Seu dobro é: {}'.format(dobro))
+print('Seu triplo é: {}'.format(triplo))
+print('Raiz quadrada é {:.3f}'.format(raiz))
