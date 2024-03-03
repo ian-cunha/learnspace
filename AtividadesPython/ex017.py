8baf3735efd615f0e863ef2686aadea188e484b6
Ian <iancunha20@gmail.com>
2024-03-03 18:25:03 -0300

    update

--- AtividadesPython/ex017.py
+++ AtividadesPython/ex017.py
@@ -2,3 +2,8 @@
 # cateto adjacente de um triângulo retângulo, calcule e mostre o
 # comprimento da hipotenusa.
 
+from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
