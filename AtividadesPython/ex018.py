059d9cb24f0f64146a1d6c6c4e6284975fcec3c6
Ian <iancunha20@gmail.com>
2024-03-03 18:35:02 -0300

    update

--- AtividadesPython/ex018.py
+++ AtividadesPython/ex018.py
@@ -1,3 +1,11 @@
 # Faça um programa que leia um ângulo qualquer
 # e mostre na tela o valor do seno, consseno e
 # tangente desse ângulo.
+
+from math import radians, sin, cos, tan
+ang = float(input('Digite um ângulo: '))
+rad = radians(ang)
+sen = sin(rad)
+cos = cos(rad)
+tang = tan(rad)
+print('Seno {:.2f},\nCosseno {:.2f},\nTangente {:.2f}'.format(sen, cos, tang))
