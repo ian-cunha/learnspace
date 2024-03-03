133a314a1467d912d3949cd359695822aef73322
Ian <iancunha20@gmail.com>
2024-03-03 17:51:52 -0300

    update

--- AtividadesPython/ex016.py
+++ AtividadesPython/ex016.py
@@ -1,2 +1,11 @@
 # Crie um programa que leia um número Real qualquer e mostre na tela a sua porção inteira.
 # Ex: O número 6.152, tem sua porção intera de 6.
+
+'''
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
+'''
+
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
--- AtividadesPython/ex017.py
+++ AtividadesPython/ex017.py
@@ -1,3 +1,4 @@
 # Faça um programa que leia o comprimento de um cateto oposto e do
 # cateto adjacente de um triângulo retângulo, calcule e mostre o
 # comprimento da hipotenusa.
+
