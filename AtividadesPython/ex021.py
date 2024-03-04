c5331c51f32983d4e05759d46bf2f9743fa58b0f
Ian <iancunha20@gmail.com>
2024-03-03 21:30:47 -0300

    update

--- AtividadesPython/ex021.py
+++ AtividadesPython/ex021.py
@@ -1,2 +1,10 @@
 # Faça um programa em Python que abra e reproduza o
 # aúdio de um arquivo MP3.
+
+import pygame
+import os
+print(os.getcwd)
+pygame.init()
+pygame.mixer.music.load(os.path.join(os.path.dirname(__file__), 'ex021.mp3'))
+pygame.mixer.music.play()
+pygame.event.wait()
