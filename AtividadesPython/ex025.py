da1c06d4154863bcce0a66ed3069ad9e08eb7508
Ian <iancunha20@gmail.com>
2024-03-05 03:15:27 -0300

    update

--- AtividadesPython/ex025.py
+++ AtividadesPython/ex025.py
@@ -1,2 +1,5 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem
 # 'Silva' no nome.
+
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
