6adef047e16e1784b5b16ad91bfdf50825a455c0
Ian <iancunha20@gmail.com>
2024-03-05 19:20:16 -0300

    update

---
+++ AtividadesPython/ex029.py
@@ -0,0 +1,3 @@
+# Escreva um programa que leia a velocidade de um carro.
+# Se ele ultrapassar 80km/h, mostre uma  mensagem dizendo que ele foi multado.
+# a multa vai custar R$7,00 por cada Km acima do limite.
