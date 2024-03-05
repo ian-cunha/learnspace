135e00d147f5724f01248f1b99353b81c483230c
Ian <iancunha20@gmail.com>
2024-03-05 03:06:49 -0300

    update

--- AtividadesPython/ex024.py
+++ AtividadesPython/ex024.py
@@ -1,6 +1,5 @@
 # Crie um programa que leia um nome de uma cidade e diga se ela
 # começa ou não com o nome 'Santo'.
 
-cidade = str(input('Cidade: '))
-analise = 'Santo' in cidade
-print(analise)
+cidade = str(input('Em que cidade você nasceu? ')).strip()
+print(cidade[:5].upper() == 'SANTO')
