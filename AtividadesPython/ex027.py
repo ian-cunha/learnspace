4bf421d2de52429f496ac27fe4b0d8468fff4f9b
Ian <iancunha20@gmail.com>
2024-03-05 03:34:57 -0300

    Update ex027.py

--- AtividadesPython/ex027.py
+++ AtividadesPython/ex027.py
@@ -3,3 +3,9 @@
 # Ex: Ana Maria de Souza
 # primeiro = Ana
 # ultimo = Souza
+
+n = str(input('Qual o seu nome completo? ')).strip()
+nome = n.split()
+print('Bem-vindo!')
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu último nome é {}'.format(nome[len(nome)-1]))
