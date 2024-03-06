e9523c53c06463a78588d8d111a0faf2455ec9b2
Ian <iancunha20@gmail.com>
2024-03-05 23:46:31 -0300

    update

--- AtividadesPython/ex029.py
+++ AtividadesPython/ex029.py
@@ -2,8 +2,9 @@
 # Se ele ultrapassar 80km/h, mostre uma  mensagem dizendo que ele foi multado.
 # a multa vai custar R$7,00 por cada Km acima do limite.
 
-veloc = int(input('Velocidade do carro: '))
-if veloc > 80:
-    print('Você foi multado!')
-else:
-    print('Você não foi multado!')
+velocidade = float(input('Velocidade que o carro passou: '))
+if velocidade > 80.0:
+    print('MULTADO! Você excedeu o limite permitido que é 80Km/h')
+    multa = (velocidade-80) * 7
+    print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
+print('Tenha um boa dia! Dirija com segurança!')
