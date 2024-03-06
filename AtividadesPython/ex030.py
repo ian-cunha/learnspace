b35faadc91da8d08aae10069a6c62926186c252e
Ian <iancunha20@gmail.com>
2024-03-05 23:55:34 -0300

    update

--- AtividadesPython/ex030.py
+++ AtividadesPython/ex030.py
@@ -1,4 +1,8 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou Impar.
 
 num = int(input('Digite um número: '))
-print(num)
+analise = num % 2
+if analise == 0:
+    print('O número {} é PAR.'.format(num))
+else:
+    print('O número {} é ÍMPAR.'.format(num))
