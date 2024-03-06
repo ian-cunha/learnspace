bd241179f98f2b2af22c7ad5682db9e93c8d5e89
Ian <iancunha20@gmail.com>
2024-03-05 23:02:56 -0300

    Update ex028.py

--- AtividadesPython/ex028.py
+++ AtividadesPython/ex028.py
@@ -2,3 +2,11 @@
 # entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido
 # pelo computador.
 # O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+from random import randint
+num = int(randint(0, 5))
+escolha = int(input('Digite um número entre 0 e 5: '))
+if escolha == num:
+    print('Você acertou!')
+else:
+    print('Você errou, tente novamente!')
