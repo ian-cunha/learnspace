3d4f7efcba100f4f9aef222cf718ced99e533934
Ian <iancunha20@gmail.com>
2024-03-08 00:58:28 -0300

    update

--- AtividadesPython/ex037.py
+++ AtividadesPython/ex037.py
@@ -3,3 +3,18 @@
 # -1 para binário
 # -2 para octal
 # -3 para hexadecimal
+
+num = int(input('Digite um número inteiro: '))
+print('Escolha uma das base para conversão:')
+print('[ 1 ] converter para BINÁRIO')
+print('[ 2 ] converter para OCTAL')
+print('[ 3 ] converter para HEXADECIMAL')
+base = int(input('Sua opção: '))
+if base == 1:
+    print('{} convertido em BINÁRIO é {}'.format(num, bin(num)))
+elif base == 2:
+    print('{} convertido em OCTAL é {}'.format(num ,oct(num)))
+elif base == 3:
+    print('{} convertido em HEXADECIMAL é {}'.format(num ,hex(num)))
+else:
+    print('Tente novamente você escolheu uma base inexistente.')
