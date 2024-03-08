15841254cc6141351767cc23b7b2c1e62cd2557b
Ian <iancunha20@gmail.com>
2024-03-08 01:58:28 -0300

    Update ex039.py

--- AtividadesPython/ex039.py
+++ AtividadesPython/ex039.py
@@ -4,3 +4,21 @@
 # - Se é a hora de se alistar.
 # - Se já passou do tempo do alistamento.
 # Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+from datetime import date
+atual = date.today().year
+nasc = int(input('Ano de nascimento: '))
+idade = atual - nasc
+print('Quen nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade == 18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+    ano = atual + saldo
+    print('Seu alistamento será em {}'.format(ano))
+else:
+    saldo = idade - 18
+    print('Você já deveria ter se alistado há {} anos.'.format(saldo))
+    ano = atual - saldo
+    print('Seu alistamento foi em {}'.format(ano))
