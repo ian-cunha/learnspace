8e9630f9d13cf3c415b6d4f0335db136b0589a33
Ian <iancunha20@gmail.com>
2024-03-07 21:08:44 -0300

    update

---
+++ AtividadesPython/ex042.py
@@ -0,0 +1,5 @@
+# Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo
+# de triângulo será formado:
+# - Equilátero: todos os lados iguais
+# - Isósceles: dois iguais
+# - Escaleno: todos os lados diferentes
