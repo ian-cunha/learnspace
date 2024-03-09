9f6efce664b4d60eeb26d29d140b33bb12fde779
Ian <iancunha20@gmail.com>
2024-03-09 03:30:06 -0300

    Update ex042.py

--- AtividadesPython/ex042.py
+++ AtividadesPython/ex042.py
@@ -3,3 +3,20 @@
 # - Equilátero: todos os lados iguais
 # - Isósceles: dois iguais
 # - Escaleno: todos os lados diferentes
+
+print('-=-' * 20)
+print('Analisando triângulos')
+print('-=-' * 20)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os sementos PODEM formar um triângulo ', end='')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO')
+    else:
+        print('ISÓSCELES')
+else:
+    print('Os segmentos NÃO PODEM formar um triângulo')
