47cc86d934e78de404f41f3ebc9339dac5cc4442
Ian <iancunha20@gmail.com>
2024-03-09 03:07:34 -0300

    Update ex041.py

--- AtividadesPython/ex041.py
+++ AtividadesPython/ex041.py
@@ -3,5 +3,21 @@
 # - Até 9 anos: MIRIM
 # - Até 14 anos: INFANTIL
 # - Até 19 anos: JUNIOR
-# - Até 20 anos: SÊNIOR
+# - Até 25 anos: SÊNIOR
 # - Acima: MASTER
+
+from datetime import date
+atual = date.today().year
+ano = int(input('Ano de nascimento: '))
+idade = atual - ano
+print('O atleta tem {} anos.'.format(idade))
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade <= 19:
+    print('Classificação: JUNIOR')
+elif idade <= 25:
+    print('Classificação: SÊNIOR')
+else:
+    print('Classificação: MASTER')
