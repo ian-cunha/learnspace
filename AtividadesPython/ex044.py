72ddc4aff68fbe0106c88cf349c4a86b04a61acf
Ian <iancunha20@gmail.com>
2024-03-10 16:29:31 -0300

    update

--- AtividadesPython/ex044.py
+++ AtividadesPython/ex044.py
@@ -5,24 +5,28 @@
 # - Em até 2x no cartão: preço normal
 # - 3x ou mais no cartão: 20% de juros
 
-print('-' * 20, 'STORE', '-' * 20)
+print('{:-^40}'.format(' STORE '))
 valor = float(input('Preço das compras R$'))
 print('''FORMAS DE PAGAMENTO
 [ 1 ] à vista dinheiro/cheque
 [ 2 ] à vista no cartão
 [ 3 ] 2x no cartão
-[ 4 ] 3x ou mais no cartão
-''')
+[ 4 ] 3x ou mais no cartão''')
 tipo = int(input('Qual é a opção? '))
 if tipo == 1:
     total = valor - (valor * 10 / 100)
-    print('Sua compra de R${} vai custar R${} no final.'.format(valor, total))
 elif tipo == 2:
     total = valor - (valor * 5 / 100)
-    print('Sua compra de R${} vai custar R${} no final.'.format(valor, total))
 elif tipo == 3:
-    print('Sua compra de R${} vai custar R${} no final.'.format(valor, valor))
+    total = valor
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de R${:.2f} sem juros.'.format(parcela))
 elif tipo == 4:
     total = valor + (valor * 20 / 100)
-    print('Sua compra de R${} vai custar R${} no final.'.format(valor, total))
-# Incompleto
+    totparc = int(input('Quantas parcelas? '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de R${:.2f} com juros.'.format(totparc, parcela))
+else:
+    total = valor
+    print('Opção inválida de pagamento. Tente novamente!')
+print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(valor, total))
--- AtividadesPython/ex045.py
+++ AtividadesPython/ex045.py
@@ -1,2 +1,28 @@
 # Crie um programa que faça o computador jogar Jokenpô com você.
 # Pedra, papel e tesoura.
+
+from random import randint
+print('{:=^20}'.format(' JOKENPÔ '))
+print('''Escolha entre:
+[ 1 ] Pedra
+[ 2 ] Papel
+[ 3 ] Tesoura''')
+computador = randint(1, 3)
+jogador = int(input('Qual sua escolha? '))
+if jogador == 1 and computador == 3:
+    print('Você ganhou!')
+    print('Eu escolhi: {}'.format(computador))
+elif jogador == 2 and computador == 1:
+    print('Você ganhou!')
+    print('Eu escolhi: {}'.format(computador))
+elif jogador == 3 and computador == 2:
+    print('Você ganhou!')
+    print('Eu escolhi: {}'.format(computador))
+elif jogador == computador:
+    print('Empate')
+    print('Eu também escolhi: {}'.format(computador))
+elif jogador > 3:
+    print('Opção inválida, tente novamente!')
+else:
+    print('Você perdeu!')
+    print('Eu escolhi: {}'.format(computador))
