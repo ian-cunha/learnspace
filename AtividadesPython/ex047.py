df6cb41d1a234dd9f8d73f9dc0798493d1eb18fa
Ian <iancunha20@gmail.com>
2024-03-11 17:40:53 -0300

    Update ex047.py

--- AtividadesPython/ex047.py
+++ AtividadesPython/ex047.py
@@ -1,7 +1,11 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo
 # entre 1 e 50.
 
-for contador in range(1, 51):
-    num = contador % 2
-    if num == 0:
-        print(contador)
+for cont in range(2, 51, 2):
+    print(cont, end=' ')
+print('Fim!')
+
+#for cont in range(1, 51):
+#    if cont % 2 == 0:
+#        print(cont, end=', ')
+#print('Fim!')
