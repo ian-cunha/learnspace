695c986ac36ad8e09a45ff5164731eb48d0686ad
Ian <iancunha20@gmail.com>
2024-03-11 00:25:06 -0300

    update

---
+++ AtividadesPython/ex053.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia uma frase qualquer e diga se ela é um PALINDROMO,
+# desconsiderando os espaços.
+# Ex: APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, OO LOBO AMA O BOLO
+# ANOTARAM A DATA DA MARATONA.
---
+++ TestesPython/aula13.py
@@ -0,0 +1,24 @@
+#for c in range(0, 20, 5):
+#    print(c)
+#print('FIM')
+
+#for c in range(0, 5):
+#    print('Teste')
+#print('FIM')
+
+#n = int(input('Digite um número: '))
+#for c in range(0, n+1):
+#    print(c)
+
+#i = int(input('Inicio: '))
+#f = int(input('Fim: '))
+#p = int(input('Passo: '))
+#for c in range(i, f+1, p):
+#    print(c)
+#print('FIM')
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatório de todos os valores foi {}'.format(s))
