200887aebb5ac832540943bae8b92fdc35ba81eb
Ian <iancunha20@gmail.com>
2024-03-12 01:01:02 -0300

    update

--- AtividadesPython/ex053.py
+++ AtividadesPython/ex053.py
@@ -1,4 +1,25 @@
 # Crie um programa que leia uma frase qualquer e diga se ela é um PALINDROMO,
 # desconsiderando os espaços.
-# Ex: APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, OO LOBO AMA O BOLO
+# Ex: APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO
 # ANOTARAM A DATA DA MARATONA.
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+inverso = junto[::-1]
+print('O inverso é {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
+
+'''
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+print('O inverso é {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
+'''
--- AtividadesPython/ex054.py
+++ AtividadesPython/ex054.py
@@ -1,3 +1,17 @@
 # Crie um programa que leia o ano de nascimento de sete pessoas. No final,
 # mostre quantas pessoas ainda não atingiram a maioridade e quantas
 # jã são maiores.
+
+from datetime import date
+atual = date.today().year
+maior = 0
+menor = 0
+for pess in range(1, 8):
+    nasc = int(input('Em que ano a {}º pessoa nasceu? '.format(pess)))
+    idade = atual - nasc
+    if idade >= 18:
+        maior += 1
+    else:
+        menor += 1
+print('Ao todo tivemos {} pessoas maiores de idade'.format(maior))
+print('E também tivemos {} pessoas menores de idade'.format(menor))
