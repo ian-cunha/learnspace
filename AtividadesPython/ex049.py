77ee9b9904a19d17ce5a7675626af5d00e9c759f
Ian <iancunha20@gmail.com>
2024-03-11 22:44:32 -0300

    Update ex049.py

--- AtividadesPython/ex049.py
+++ AtividadesPython/ex049.py
@@ -2,7 +2,5 @@
 # só que agora utilizando um laço for.
 
 n = int(input('Tabuada de: '))
-print('-' * 15)
-for tabuada in range(1,11):
-    print('{} x {:2} = {}'.format(n, tabuada, n * tabuada))
-print('-' * 15)
+for c in range(1,11):
+    print('{} x {:2} = {}'.format(n, c, n * c))
