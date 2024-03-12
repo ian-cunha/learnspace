0adbfbae6d826ff0f2889be957e2f58bd6df55c5
Ian <iancunha20@gmail.com>
2024-03-11 23:01:26 -0300

    Update ex050.py

--- AtividadesPython/ex050.py
+++ AtividadesPython/ex050.py
@@ -8,4 +8,4 @@ for c in range(1, 7):
     if num % 2 == 0:
         soma += num
         cont += 1
-print('Você informou {} números e a soma foi {}'.format(cont, soma))
+print('Você informou {} números PARES e a soma foi {}'.format(cont, soma))
