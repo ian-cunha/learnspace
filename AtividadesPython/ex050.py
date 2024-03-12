e780de576e72cd971acd986a4b3dae5dcb0cea46
Ian <iancunha20@gmail.com>
2024-03-11 22:56:05 -0300

    Update ex050.py

--- AtividadesPython/ex050.py
+++ AtividadesPython/ex050.py
@@ -2,7 +2,8 @@
 # daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
 soma = 0
-for num in range(1,7):
-    n = int(input('Escolha {} número inteiro: '.format(num)))
-    soma = soma + n
-print('O Total será: {}'.format(soma))
+for c in range(1, 7):
+    num = int(input('Escolha {} número inteiro: '))
+    if num % 2 == 0:
+        soma += num
+print('A soma de todos os pares é {}'.format(soma))
