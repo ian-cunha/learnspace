f5992877cf255cff3c7cf3a87034bd20ef96e90e
Ian <iancunha20@gmail.com>
2024-03-12 00:17:02 -0300

    Update ex052.py

--- AtividadesPython/ex052.py
+++ AtividadesPython/ex052.py
@@ -1,2 +1,17 @@
 # Faça um programa que leia um número inteiro e diga se ele é ou não
 # um número primo.
+
+num = int(input('Digite um número: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\33[33m', end='')
+        tot += 1
+    else:
+        print('\33[31m', end='')
+    print('{} '.format(c), end='')
+print('\n\033[mO número {} foi divisível {} vezes'.format(num, tot))
+if tot == 2:
+    print('E por isso ele \33[33mÉ PRIMO!\033[m')
+else:
+    print('E por isso ele \33[31mNÃO É PRIMO!\033[m')
