978d9bed6353799f4dc81216cd7eab06f454af17
Ian <iancunha20@gmail.com>
2024-03-12 15:31:06 -0300

    update

--- AtividadesPython/ex060.py
+++ AtividadesPython/ex060.py
@@ -1,3 +1,9 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial.
 # Ex: 5!=5x4x3x2x1=120
 # Fazer com FOR e WHILE
+
+num = int(input('Digite um número para calcular o seu Fatorial: '))
+
+#from math import factorial
+#num = int(input('Digite um número para calcular o Fatorial: '))
+#print('O fatorial de {} è {}'.format(num, fat))
