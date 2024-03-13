90cd9fb6e217185cf845bc03df34c7fae4b3cf19
Ian <iancunha20@gmail.com>
2024-03-13 20:09:31 -0300

    Update ex065.py

--- AtividadesPython/ex065.py
+++ AtividadesPython/ex065.py
@@ -2,3 +2,21 @@
 # execução, mostre a média entre todos os valores e qual foi a maior e menor
 # valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar
 # a digitar valores.
+
+resp = 'S'
+soma = qtd = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    qtd += 1
+    if qtd == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+media = soma / qtd
+print('Você digitou {} números e a média foi {:.2f}'.format(qtd, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
