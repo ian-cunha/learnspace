47fc2d6bcab0da40739f8038396e3b894d49f6b1
Ian <iancunha20@gmail.com>
2024-03-13 17:24:43 -0300

    Update ex061.py

--- AtividadesPython/ex061.py
+++ AtividadesPython/ex061.py
@@ -1,2 +1,16 @@
 # Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os
 # 10 primeiros termos da progressão usando a estrutura while.
+
+print('=' * 20)
+print('10 TERMOS DE UMA PA')
+print('=' * 20)
+
+num = int(input('Primeiro termo: '))
+raz = int(input('Razão: '))
+termo = num
+cont = 1
+while cont <= 10:
+    print('{}'.format(termo), end=' => ')
+    termo += raz
+    cont += 1
+print('Fim!')
