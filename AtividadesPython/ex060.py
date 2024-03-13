3eab7b1342217040f5a485eb58df85d8b16ba677
Ian <iancunha20@gmail.com>
2024-03-13 17:06:47 -0300

    Update ex060.py

--- AtividadesPython/ex060.py
+++ AtividadesPython/ex060.py
@@ -2,7 +2,16 @@
 # Ex: 5!=5x4x3x2x1=120
 # Fazer com FOR e WHILE
 
-num = int(input('Digite um número para calcular o seu Fatorial: '))
+n = int(input('Digite um número para calcular o seu Fatorial: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
 
 #from math import factorial
 #num = int(input('Digite um número para calcular o Fatorial: '))
