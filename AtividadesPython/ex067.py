0397331badef2bb79a81818bbf3b84e2c4bac721
Ian <iancunha20@gmail.com>
2024-03-15 02:57:50 -0300

    otimization

--- AtividadesPython/ex067.py
+++ AtividadesPython/ex067.py
@@ -2,15 +2,12 @@
 # valor digitado pelo usuário. O programa será interrompido quando o número solicitado
 # for negativo.
 
-num = cont = result = 0
 while True:
     num = int(input('Tabuada de: '))
-    if num == 999:
+    if num < 0:
         break
     print('=-'*8)
-    for cont in range(0,10):
-        cont += 1
-        result = num * cont
-        print(f'{num} x {cont} = {result}')
+    for cont in range(1, 11):
+        print(f'{num} x {cont} = {num * cont}')
     print('-='*8)
-print('Finalizado.')
+print('Programa de tabuada encerrado, volte sempre!')
