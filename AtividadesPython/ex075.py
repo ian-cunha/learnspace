c8ff49b9a43b69bd4de84823f0552dfc2cc9346b
Ian <iancunha20@gmail.com>
2024-03-16 19:12:21 -0300

    update

--- AtividadesPython/ex075.py
+++ AtividadesPython/ex075.py
@@ -3,3 +3,22 @@
 # A) Quantas vezes apareceu o valor 9.
 # B) Em que posição foi digitado o primeiro valo 3.
 # C) Quais foram os números pares.
+
+par = 0
+primeiro = int(input('Digite um número: '))
+segundo = int(input('Outro número: '))
+terceiro = int(input('Mais um número: '))
+quarto = int(input('O último número: '))
+valores = (primeiro, segundo, terceiro, quarto)
+if primeiro % 2 == 0:
+    par += 1
+if segundo % 2 == 0:
+    par += 1
+if terceiro % 2 == 0:
+    par += 1
+if quarto % 2 == 0:
+    par += 1
+print(f'Você digitou os valores {valores}')
+print(f'O valor 9 apareceu {valores.count(9)} vezes')
+print(f'O valor 3 apareceu na {valores.count(3)}ª posição')
+print(f'Os valores pares digitados foram {par}')
