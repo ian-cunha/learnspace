395afc02262a3b5670959bcbf9e20d7976a46d4e
Ian <iancunha20@gmail.com>
2024-03-16 19:46:34 -0300

    Update ex077.py

--- AtividadesPython/ex077.py
+++ AtividadesPython/ex077.py
@@ -1,2 +1,6 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 # Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+
+palavras = ('APRENDER', 'PROGRAMAR', ' LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', ' PROGRAMADOR', 'FUTURO')
+vogais = ('a', 'e', 'i', 'o', 'u')
+print(f'Na palavra {palavras} temos {vogais}')
