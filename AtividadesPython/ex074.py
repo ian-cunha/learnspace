471f822f94a22b4b16a9bc9d3cfb9c3e2d47a628
Ian <iancunha20@gmail.com>
2024-03-18 04:52:04 -0300

    Update ex074.py

--- AtividadesPython/ex074.py
+++ AtividadesPython/ex074.py
@@ -1,3 +1,11 @@
 # Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
 # Depois disso, mostre a listagem de números gerados e também indique o menos e o maior
 # valor que estão na tupla.
+from random import randint
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), 
+    randint(1, 10), randint(1, 10))
+print('Os valores sorteados foram: ', end='')
+for n in numeros:
+    print(f'{n} ', end='')
+print(f'\nO maior valor sorteado foi {max(numeros)}')
+print(f'o menor valor sorteado foi {min(numeros)}')
