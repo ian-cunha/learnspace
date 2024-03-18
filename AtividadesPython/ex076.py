3c92d440126a83ffc0ad7be0eab4652f0c58a5ec
Ian <iancunha20@gmail.com>
2024-03-18 05:26:13 -0300

    Update ex076.py

--- AtividadesPython/ex076.py
+++ AtividadesPython/ex076.py
@@ -2,12 +2,18 @@
 # preços, na sequência. No final, mostre uma listagem de preços, organizando os dados
 # em forma tabular.
 
-produtos = ('Notebook DELL i7 9gen', 5500, 'Mouse AJAZZ 199', 250, 'Controle XBOX Series X', 620, 'Galaxy S23 Ultra', 4200, 'Nintendo SWITCH Oled', 2100, 'PS5 Slim', 4500, 'CoolerMaster Torre V2', 150, 'Cabo USB-C', 25)
-cont = 0
-print('-'*42)
-print(f'{'LISTAGEM DE PREÇOS'}')
-print('-'*42)
-for cont in range(0, 15, 2):
-    print(f'{produtos[cont]:30}', end='')
-    print(f'R$ {produtos[cont+1]:8.2f}')
-print('-'*42)
+produtos = ('Gabinete CoolerMaster', 550, 
+            'Mouse AJAZZ 199', 250, 
+            'Controle XBOX Series X', 620, 
+            'Teclado RoyalKrudge V2', 320,
+            'HeadSet JBL Quantum', 720, 
+            'Cabo USB-C', 25)
+print('-'*40)
+print(f'{'LISTAGEM DE PREÇOS':^40}')
+print('-'*40)
+for item in range(0, len(produtos)):
+    if item % 2 == 0:
+        print(f'{produtos[item]:.<30}', end='')
+    else:
+        print(f'R${produtos[item]:>7.2f}')
+print('-'*40)
