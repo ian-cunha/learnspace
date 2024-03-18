dbeaecb35d118aa4e6c11bf9b9c16cf7578e50f9
Ian <iancunha20@gmail.com>
2024-03-18 03:24:03 -0300

    Update ex072.py

--- AtividadesPython/ex072.py
+++ AtividadesPython/ex072.py
@@ -2,12 +2,18 @@
 # de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
-extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quartoze', 'quinze', 'dezeseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
+extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',
+ 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quartoze', 'quinze', 
+ 'dezeseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
 while True:
     n = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= n <= 20:
+        print(f'Você digitou o número {extenso[n]}')
     if n > 20 or n < 0:
-        n = int(input('Tente novamente. Digite um número entre 0 e 20: '))
+        print('Tente novamente, número não listado! ')
+    pergunta = ' '
+    while pergunta not in 'SN':
+        pergunta = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if pergunta == 'N':
+        print('Finalizado.')
         break
-    else:
-        break
-print(f'Você digitou o número {extenso[n]}')
